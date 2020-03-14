be5056c023d3e3caa49005f760c1d72c6079fae3
EdwardMuturi <kakaturi300@gmail.com>
2020-03-14 11:06:47 +0300

    add initial files

---
+++ app/src/main/java/com/memento_guy/sare/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.memento_guy.sare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
