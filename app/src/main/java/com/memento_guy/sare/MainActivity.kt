3d56be0f4259fc459c119115ad52199c947e3a10
EdwardMuturi <kakaturi300@gmail.com>
2020-03-15 07:54:06 +0300

    show bottom sheet dialog using support fragment manager; **should be replace with nav component**

--- app/src/main/java/com/memento_guy/sare/MainActivity.kt
+++ app/src/main/java/com/memento_guy/sare/MainActivity.kt
@@ -2,14 +2,22 @@ package com.memento_guy.sare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import com.memento_guy.sare.ui.SummaryFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.memento_guy.sare.ui.expenses.ExpensesFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        BottomSheetBehavior.from(bottom_container).state = BottomSheetBehavior.STATE_COLLAPSED
+        supportFragmentManager.beginTransaction().replace(
+            R.id.bottom_container, ExpensesFragment()
+        ).commit()
     }
 }
---
+++ app/src/main/java/com/memento_guy/sare/ui/expenses/ExpensesFragment.kt
@@ -0,0 +1,31 @@
+package com.memento_guy.sare.ui.expenses
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+import com.memento_guy.sare.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ExpensesFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_expenses, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
