4e388ca0a79106b01cc0feff0267fbc872815daa
EdwardMuturi <kakaturi300@gmail.com>
2020-03-22 12:37:57 +0300

    Switch between expensesFragment and addExpenseFragment
    - use support fragment manager to switch between fragments since, currently there is an error with Nav Component;
    a way to start both Summary and ExpenseFragment so that all fragments are started by the Nav Component
    - change expense fab icon to add or done when in Expense and AddExpense fragments(s)
    - create basic layout for add expenses fragment

--- app/src/main/java/com/memento_guy/sare/ui/expenses/AddExpenseFragment.kt
+++ app/src/main/java/com/memento_guy/sare/ui/expenses/AddExpenseFragment.kt
@@ -1,14 +1,13 @@
 package com.memento_guy.sare.ui.expenses
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-
 import com.memento_guy.sare.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class AddExpenseFragment : BottomSheetDialogFragment() {
 
@@ -27,6 +26,15 @@ class AddExpenseFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        activity?.fab_add_expense?.setImageResource(R.drawable.ic_done_black_24dp)
+
+        activity?.fab_add_expense?.setOnClickListener {
+            it.fab_add_expense?.setImageResource(R.drawable.ic_add_expense_24dp)
+            requireActivity().supportFragmentManager.beginTransaction().replace(
+                R.id.bottom_container, ExpensesFragment()
+            ).commit()
+        }
     }
 
 }
