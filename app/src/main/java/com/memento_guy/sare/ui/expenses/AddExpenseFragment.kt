28ec5a71013ec48cc0a483d596208eee3acee109
EdwardMuturi <kakaturi300@gmail.com>
2020-03-22 08:47:49 +0300

    create add expense layot

---
+++ app/src/main/java/com/memento_guy/sare/ui/expenses/AddExpenseFragment.kt
@@ -0,0 +1,32 @@
+package com.memento_guy.sare.ui.expenses
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+import com.memento_guy.sare.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class AddExpenseFragment : BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_expense, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
